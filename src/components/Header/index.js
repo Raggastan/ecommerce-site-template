e749e5e82bce6d1967957bdb1c5385e760a76b17
raggastan <lincarvillais@gmail.com>
2020-02-27 23:37:28 +0800

    Edited header
    Now refers to "room 20-05"

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -91,7 +91,7 @@ class Header extends Component {
       <header className="header">
         <h1>
           <Link onlyActiveOnIndex={true} to="/" className="logo">
-            Team Store
+            Room 20-05
           </Link>
         </h1>
         {this.state.menuActive ? this.menuButton: ""}
